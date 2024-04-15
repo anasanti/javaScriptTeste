ba24ae0861785bc932221501c2a019862352be41
Ana Carolina <acpsfun@gmail.com>
2024-04-15 19:47:07 -0300

    Create variaveis
    aula 1

---
+++ variaveis.js
@@ -0,0 +1,25 @@
+// pode ser alterado string boolean
+var nome = 'Ana'
+
+// pode ser alterado float int
+let idade = 20
+idade = 32
+//não pode ser alterado 
+const sexo = 'feminino'
+
+//arrar lista de dados
+const paises = ['Alemanha', 'Brasil', 'Canada']
+
+//objeto recebe chave e valor
+let devices = {
+    nome: 'celular android',
+    preco: 1500.00,
+    cores: {
+        cor1: 'Vermelho',
+        cor2: 'Azul'
+    }
+}
+
+//fução
+
+console.log(nome, idade, sexo, paises, devices)
